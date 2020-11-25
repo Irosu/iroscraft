d96f5a41352d8146928ca470e7eae2988e3acc83
Irosu <Irosu@users.noreply.github.com>
2020-11-25 23:17:14 +0100

    Tarta de manzana funcionando.
    Se crea además una clase base para todas las tartas a la que se le podrán añadir mecánicas especiales.

--- src/main/java/com/irosu/iroscraft/util/RegistryHandler.java
+++ src/main/java/com/irosu/iroscraft/util/RegistryHandler.java
@@ -1,10 +1,7 @@
 package com.irosu.iroscraft.util;
 
 import com.irosu.iroscraft.Iroscraft;
-import com.irosu.iroscraft.blocks.BlockItemBase;
-import com.irosu.iroscraft.blocks.FulguriteOre;
-import com.irosu.iroscraft.blocks.SeranditeBlock;
-import com.irosu.iroscraft.blocks.SeranditeOre;
+import com.irosu.iroscraft.blocks.*;
 import com.irosu.iroscraft.items.ItemBase;
 import com.irosu.iroscraft.items.RainStick;
 import com.irosu.iroscraft.items.SunStick;
@@ -39,11 +36,13 @@ public class RegistryHandler {
     public static final RegistryObject<Block> SERANDITE_BLOCK = BLOCKS.register("serandite_block", SeranditeBlock::new);
     public static final RegistryObject<Block> SERANDITE_ORE = BLOCKS.register("serandite_ore", SeranditeOre::new);
     public static final RegistryObject<Block> FULGURITE_ORE = BLOCKS.register("fulgurite_ore", FulguriteOre::new);
+    public static final RegistryObject<Block> APPLE_CAKE = BLOCKS.register("apple_cake", CakeBase::new);
 
     //Block Items
     public static final RegistryObject<Item> SERANDITE_BLOCK_BI = ITEMS.register("serandite_block", () -> new BlockItemBase(SERANDITE_BLOCK.get()));
     public static final RegistryObject<Item> SERANDITE_ORE_BI = ITEMS.register("serandite_ore", () -> new BlockItemBase(SERANDITE_ORE.get()));
     public static final RegistryObject<Item> FULGURITE_ORE_BI = ITEMS.register("fulgurite_ore", () -> new BlockItemBase(FULGURITE_ORE.get()));
+    public static final RegistryObject<Item> APPLE_CAKE_BI = ITEMS.register("apple_cake", () -> new BlockItemBase(APPLE_CAKE.get()));
 
     //Tools
     public static final  RegistryObject<AxeItem> ENTHERITE_AXE = ITEMS.register("entherite_axe",
